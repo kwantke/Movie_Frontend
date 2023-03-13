f0116b5f1afbb50b136afb3edad93b4504fb317f
kokk <kokk7310@gmail.com>
2023-03-13 20:54:10 +0900

    movieDetail 데이터 가져오기 셋팅

--- src/backend/movie/RunMovieDetail.tsx
+++ src/backend/movie/RunMovieDetail.tsx
@@ -1,8 +1,28 @@
+import {useEffect} from "react";
+import axios from "axios";
 
 interface IMovie{
   movieId: string
 }
 export default function RunMovieDetail({movieId}:IMovie){
   console.log("movieId="+ movieId)
+  const movieDetailUrl = process.env.REACT_APP_MOVIE_DETAIL as string;
+  useEffect(()=>{
+    axios.get(movieDetailUrl,{
+      params:{
+        id:movieId
+      }
+    })
+    .then(function(res){
+      console.log(res);
+    })
+    .catch(function(error){
+      console.log(error)
+    })
+    .finally(function(){
+
+    });
+  })
   return <></>
+
 }
